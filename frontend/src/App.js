15678e6ff832116ba44aa721cd67830de025da80
GiovaniKill <giovanikill@hotmail.com>
2024-02-15 14:31:12 -0400

    feat: words receive accentuation after getting it right, reveal animations improved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import sixLetterWords from './six_letter_words';
 const App = () => {
   const [attemptNumber, setAttemptNumber] = useState(0);
   const [message, setMessage] = useState('');
-  const [evaluation, setEvaluation] = useState([]);
+  const [response, setResponse] = useState({evaluation: [], accentuatedAnswer: ''});
 
   const checkAttempt = (attempt) => {
     if(attempt.length < 6){
@@ -21,18 +21,19 @@ const App = () => {
     }
 
     requestData(`check/${attempt.toLowerCase()}`)
-    .then(({ evaluation }) => {
-      if(evaluation.every((elem) => elem === 'right')){
-        setEvaluation(() => evaluation);
+    .then((response) => {
+      console.log(response);
+      if(response.evaluation.every((elem) => elem === 'right')){
+        setResponse(() => response);
         setTimeout(() => setMessage(() => 'Parabéns!'), 3000);
       } else {
-        console.log(evaluation);
-        setEvaluation(() => evaluation);
+        setResponse(() => response);
         setMessage(() => '');
         setTimeout(() => setAttemptNumber((prev) => prev + 1), 3000);
       }
     })
     .catch((error) => {
+      console.log(error);
       setMessage(() => 'Falha ao se conectar ao servidor: ' + error);
     })
   }
@@ -42,14 +43,14 @@ const App = () => {
       <div>{message}</div>
       <table>
         <tbody>
-          <Word wordNumber={0} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={1} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={2} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={3} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={4} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={5} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={6} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
-          <Word wordNumber={7} attemptNumber={attemptNumber} evaluation={evaluation}checkAttempt={checkAttempt}/>
+          <Word wordNumber={0} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={1} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={2} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={3} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={4} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={5} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={6} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
+          <Word wordNumber={7} attemptNumber={attemptNumber} response={response}checkAttempt={checkAttempt}/>
         </tbody>
       </table>
     </div>
