95ca42212c2e08a32ea6f2711d74b5a36395963e
GiovaniKill <giovanikill@hotmail.com>
2024-02-16 19:31:47 -0400

    feat: added eslint for bacnend and fixed lint problems

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,32 @@
-import express = require('express');
-import { router } from './router/router.ts';
+import express = require('express')
+import { router } from './router/router.ts'
 
 class App {
-    public app: express.Express;
+  public app: express.Express
 
-    constructor() {
-        this.app = express();
+  constructor () {
+    this.app = express()
 
-        this.config();
+    this.config()
 
-        this.app.use(router);
+    this.app.use(router)
+  }
+
+  public start (PORT: string | number): void {
+    this.app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
+  }
+
+  private config (): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH')
+      res.header('Access-Control-Allow-Headers', '*')
+      next()
     }
 
-    public start(PORT: string | number): void {
-        this.app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    }
-
-    private config():void {
-        const accessControl: express.RequestHandler = (_req, res, next) => {
-          res.header('Access-Control-Allow-Origin', '*');
-          res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-          res.header('Access-Control-Allow-Headers', '*');
-          next();
-        };
-    
-        this.app.use(express.json());
-        this.app.use(accessControl);
-      }
-    
+    this.app.use(express.json())
+    this.app.use(accessControl)
+  }
 }
 
 export { App }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
-import { App } from './app.ts';
+import { App } from './app.ts'
 
-const PORT = process.env.APP_PORT || 3001;
+const PORT = process.env.APP_PORT ?? 3001
 
-new App().start(PORT);
+new App().start(PORT)
