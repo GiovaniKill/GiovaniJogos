bbe99d0ecfd3f285fe4ffa7bd03ed547c9df0f35
GiovaniKill <giovanikill@hotmail.com>
2024-02-20 13:07:12 -0400

    style: small tweaks at main page

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -6,14 +6,14 @@ const MainPage = () => {
   return (
     <div className='bg-amber-50 h-screen'>
       <header className='flex justify-center'>
-        <h1 className='my-5 text-3xl handrawn'>Giovani Jogos</h1>
+        <h1 className='my-11 text-5xl handrawn'>Giovani Jogos</h1>
       </header>
       <nav className='grid md:grid-cols-2 lg:grid-cols-3
       gap-3 mx-3 justify-items-center'>
         <Link to='/palavrou'>
           <img
             className='rounded-lg border-2 border-solid
-          border-black hover:border-yellow-300 hover:border-dashed'
+          border-black hover:border-yellow-400 hover:border-dashed'
             src='images/palavrou_logo.jpg'
           />
         </Link>
