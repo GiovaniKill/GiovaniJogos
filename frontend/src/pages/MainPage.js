6c4e78a3b0e02c3705bbb703c46e41c798ee92f7
GiovaniKill <giovanikill@hotmail.com>
2024-02-22 18:22:12 -0400

    chore: changed images to svg

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -13,8 +13,8 @@ const MainPage = () => {
         <Link to='/palavrou'>
           <img
             className='rounded-lg border-2 border-solid
-          border-black hover:border-yellow-400 hover:border-dashed w-11/12'
-            src='images/palavrou_logo.svg'
+          border-black hover:border-yellow-400 hover:border-dashed w-1/5'
+            src='images/palavrou_logo.png'
           />
         </Link>
       </nav>
