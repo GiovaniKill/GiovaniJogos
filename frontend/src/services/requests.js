858a1edcd5301a05fae631b0c143e0e1f316b573
GiovaniKill <giovanikill@hotmail.com>
2024-02-24 11:38:24 -0400

    fix: attempt to fix request bug

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const NODE_ENV = process.env.NODE_ENV;
 
 const api = axios.create({
-  baseURL: NODE_ENV ? 'http://giovanijogos.fun/be' :
+  baseURL: NODE_ENV ? 'https://giovanijogos.fun/be' :
   `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
