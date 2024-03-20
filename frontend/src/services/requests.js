5d57bc02a28055049e045b5aa5c2e4af42d73d62
GiovaniKill <giovanikill@hotmail.com>
2024-03-20 13:16:23 -0400

    feat: implemented AI answers on front-end

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 const NODE_ENV = process.env.NODE_ENV;
 
 const api = axios.create({
-  baseURL: NODE_ENV ? 'https://giovanijogos.fun/be' :
+  baseURL: NODE_ENV === 'production' ? 'https://giovanijogos.fun/be' :
   `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
-export const requestData = async (endpoint) => {
+export const getRequest = async (endpoint) => {
   const {data} = await api.get(endpoint);
   return data;
 };
+
+export const postRequest = async (endpoint, body) => {
+  const {data} = await api.post(endpoint, body);
+  return data;
+};
+
