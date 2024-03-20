8d2966e3e6f7deed84201a9e2a2d5a36004bb403
GiovaniKill <giovanikill@hotmail.com>
2024-03-20 12:44:49 -0400

    feat: added backend for adivinheacoisa

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { type Application } from 'express'
 import { palavrouRouter } from './palavrou.routes'
+import { adivinheACoisaRouter } from './adivinheACoisa.routes'
 
 export default (app: Application): void => {
   app.use('/palavrou', palavrouRouter)
+  app.use('/adivinheacoisa', adivinheACoisaRouter)
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Palavrou from './pages/Palavrou';
 import MainPage from './pages/MainPage';
+import {AdvinheACoisa} from './pages/AdvinheACoisa';
 
 /**
  * Routing
@@ -11,6 +12,7 @@ function App() {
   return (
     <Routes>
       <Route path="/palavrou" element={ <Palavrou /> } />
+      <Route exact path="/advinheacoisa" element={ <AdvinheACoisa /> } />
       <Route exact path="/" element={ <MainPage /> } />
     </Routes>
   );
