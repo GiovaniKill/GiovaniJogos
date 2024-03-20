6296e3613cc9a591559c9817dd974348393a172a
GiovaniKill <giovanikill@hotmail.com>
2024-03-20 14:14:19 -0400

    style: added some style to the chat

--- frontend/src/pages/AdivinheACoisa.js
+++ frontend/src/pages/AdivinheACoisa.js
@@ -60,24 +60,40 @@ const AdivinheACoisa = () => {
           Advinhe a coisa
         </h1>
       </header>
-      <section ref={conversationBox}>
-        {messages.map((curr, index) => (
-          <div key={index} className=''>
-            {curr.message}
-          </div>
-        ))}
-      </section>
-      <form onSubmit={onQuestionSubmit}>
-        <input
-          type='text'
-          onChange={(e) => setTextInput(e.target.value)}
-          placeholder='Faça sua pergunta'
-          value={textInput}
-        />
-        <button type='submit' onClick={(e) => onQuestionSubmit(e)}>
+      <section
+        id='chat'
+        className='flex flex-col justify-start
+         items-center border-2 h-screen'
+      >
+        <section
+          ref={conversationBox}
+          className='w-1/2 border-2 h-3/5 flex flex-col overflow-y-scroll'
+        >
+          {messages.map((curr, index) => (
+            <div key={index}
+              className={`flex 
+              ${curr.role === 'user' ?
+              'self-end justify-end bg-blue-300 bg-opacity-50' :
+              'self-start bg-green-300 bg-opacity-50'} 
+              m-3 w-2/5 rounded-lg p-1`}
+            >
+              {curr.message}
+            </div>
+          ))}
+        </section>
+        <form onSubmit={onQuestionSubmit} className='w-1/2'>
+          <input
+            type='text'
+            onChange={(e) => setTextInput(e.target.value)}
+            placeholder='Faça sua pergunta'
+            value={textInput}
+            className='w-1/2'
+          />
+          <button type='submit' onClick={(e) => onQuestionSubmit(e)}>
           Enviar
-        </button>
-      </form>
+          </button>
+        </form>
+      </section>
     </div>
   );
 };
