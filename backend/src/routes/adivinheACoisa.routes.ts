c2c53619842201540e2f23cba9348f59165ac6eb
GiovaniKill <giovanikill@hotmail.com>
2024-03-20 14:14:19 -0400

    style: added some style to the chat

--- backend/src/routes/adivinheACoisa.routes.ts
+++ backend/src/routes/adivinheACoisa.routes.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import Service from '../service/adivinheACoisaService'
 
 const adivinheACoisaRouter = Router()
 const service = new Service()
 
-adivinheACoisaRouter.post('/ask', (req, res) => service.ask(req, res))
+adivinheACoisaRouter.post('/ask', async (req, res) => await service.ask(req, res))
 
 export { adivinheACoisaRouter }
