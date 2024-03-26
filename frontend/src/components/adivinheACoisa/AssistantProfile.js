e8744a3cedf24968c18256f2e2092f7140b724c4
GiovaniKill <giovanikill@hotmail.com>
2024-03-26 19:56:50 -0400

    style: changes in all major components

--- frontend/src/components/adivinheACoisa/AssistantProfile.js
+++ frontend/src/components/adivinheACoisa/AssistantProfile.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import {motion} from 'framer-motion';
+import PropTypes from 'prop-types';
 
-const AssistantProfile = () => {
+const AssistantProfile = ({setIsProfileActive}) => {
   return (
     <motion.section
       className='assistant-profile'
       initial={{
-        width: 0,
+        x: 50,
       }}
       animate={{
-        width: '20%',
+        x: 0,
+      }}
+      transition={{
+        type: 'spring',
+        duration: 0.5,
+        ease: 'easeInOut',
       }}
     >
+      <header className='assistant-profile-header'>
+        <button onClick={() => setIsProfileActive((curr) => !curr)}>
+          X
+        </button>
+      </header>
       <img src='images/assistant-profile-pic.svg' className='profile-pic'/>
       <p> assistant name</p>
       <p> this is the description of
           the AI assistant, wether you like it or not</p>
     </motion.section>
   );
+};
+
+AssistantProfile.propTypes = {
+  setIsProfileActive: PropTypes.bool.isRequired,
 };
 
 export {AssistantProfile};
--- frontend/src/pages/AdivinheACoisa.js
+++ frontend/src/pages/AdivinheACoisa.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
 import Chat from '../components/adivinheACoisa/Chat';
 import {AssistantProfile} from '../components/adivinheACoisa/AssistantProfile';
 import {ChatsNavBar} from '../components/adivinheACoisa/ChatsNavBar';
 
 const AdivinheACoisa = () => {
-  const [isChatNavBarOpen, setIsChatNavBarOpen] = useState(false);
   const [isProfileActive, setIsProfileActive] = useState(false);
 
 
@@ -13,11 +11,11 @@ const AdivinheACoisa = () => {
     <div className='adivinhe-a-coisa'>
       <ChatsNavBar/>
       <Chat
-        isChatNavBarOpen={isChatNavBarOpen}
         isProfileActive={isProfileActive}
         setIsProfileActive={setIsProfileActive}
       />
-      {isProfileActive && <AssistantProfile/>}
+      {isProfileActive && <AssistantProfile
+        setIsProfileActive={setIsProfileActive}/>}
     </div>
   );
 };
