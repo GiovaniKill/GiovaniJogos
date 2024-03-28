d2e8e0d4be80f8e9ec74e624742a45f6bb1f7d24
GiovaniKill <giovanikill@hotmail.com>
2024-03-28 14:15:06 -0400

    feat: added description to the assistants

--- backend/src/data/adivinheACoisa/assistants.ts
+++ backend/src/data/adivinheACoisa/assistants.ts
@@ -2,28 +2,33 @@ interface assistant {
   name: string
   instructions: string
   profilePicPath: string
+  description: string
 }
 
 interface responseAssistant {
   name: string
   profilePic: string
+  description: string
 }
 
 const assistants: assistant[] = [
   {
     name: 'Luísa',
     instructions: '',
-    profilePicPath: 'src/data/adivinheACoisa/profilePics/luisa-profile-pic.svg'
+    profilePicPath: 'src/data/adivinheACoisa/profilePics/luisa-profile-pic.svg',
+    description: 'Te ajudando a achar o segredo!'
   },
   {
     name: 'Kleber',
     instructions: '',
-    profilePicPath: 'src/data/adivinheACoisa/profilePics/kleber-profile-pic.svg'
+    profilePicPath: 'src/data/adivinheACoisa/profilePics/kleber-profile-pic.svg',
+    description: 'Iha!'
   },
   {
     name: 'Enzo',
     instructions: '',
-    profilePicPath: 'src/data/adivinheACoisa/profilePics/enzo-profile-pic.svg'
+    profilePicPath: 'src/data/adivinheACoisa/profilePics/enzo-profile-pic.svg',
+    description: 'Não enche'
   }
 ]
 
