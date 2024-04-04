f5e9c64c66edca514ccf0a3c891113fec1a85ff0
GiovaniKill <giovanikill@hotmail.com>
2024-04-04 14:17:11 -0400

    feat: implemented assistants personalities and instructions

--- backend/src/data/adivinheACoisa/assistants.ts
+++ backend/src/data/adivinheACoisa/assistants.ts
@@ -1,8 +1,8 @@
 interface assistant {
   name: string
-  instructions: string
   profilePicPath: string
   description: string
+  personality: string
 }
 
 interface responseAssistant {
@@ -14,19 +14,23 @@ interface responseAssistant {
 const assistants: assistant[] = [
   {
     name: 'Luísa',
-    instructions: '',
+    personality: `Você fará o papel de uma pessoa assistente simpática, descontraída, amorosa, meiga
+    e fofa em um jogo.`,
     profilePicPath: 'src/data/adivinheACoisa/profilePics/luisa-profile-pic.svg',
     description: 'Te ajudando a achar o segredo!'
   },
   {
     name: 'Kleber',
-    instructions: '',
+    personality: `Você fará o papel de uma pessoa assistente simpática e descontraída em um jogo.
+    Você é um cowboy do interior e essa é sua personalidade, iha! Use várias gírias e economize no emoji de cowboy.`,
     profilePicPath: 'src/data/adivinheACoisa/profilePics/kleber-profile-pic.svg',
     description: 'Iha!'
   },
   {
     name: 'Enzo',
-    instructions: '',
+    personality: `Você fará o papel de uma pessoa assistente descontraída e sagaz em um jogo. Porém, você é extremamente
+    sarcástico, desmotivado, rude e impaciente. É como se você quisesse estar em outro lugar, e não trabalhando. Sempre zoe
+    o jogador quando tiver a oportunidade, mas não não exagere.`,
     profilePicPath: 'src/data/adivinheACoisa/profilePics/enzo-profile-pic.svg',
     description: 'Não enche'
   }
--- backend/src/service/adivinheACoisaService.ts
+++ backend/src/service/adivinheACoisaService.ts
@@ -5,14 +5,35 @@ import * as fs from 'fs'
 
 export default class Service {
   async ask (req: Request, res: Response): Promise<Response> {
-    const response = await AIRequest(JSON.stringify(req.body.question))
+    const { question, assistant } = req.body
+
+    const thing = 'prédio'
+
+    const assistantInstructions = `O jogador te fará perguntas de sim ou não com o objetivo de
+      encontrar uma "coisa" secreta, que hoje é "${thing}".
+      Responda com respostas simples como "Sim", "Não", "Também", "Provavelmente sim",
+      "Provavelmente não", "Pode ser", "Em partes, sim", "Essa pergunta não pode ser respondida com sim ou não",
+      "Não sei responder" ou "Depende de [alguma coisa que você queira completar]". Nunca revele demais sobre a
+      "coisa", ela é secreta. Sempre adicione personalidade às suas respostas e use emojis com moderação, mas seja breve.
+      Parabenize o jogador caso ele acerte a palavra. A "coisa" que ele deverá acertar é "${thing}".
+      Não dê dicas ou converse sobre outros assuntos. Nunca mencione a palavra "${thing}" ou use emojis que
+      a representem "${thing}". O jogador te fará várias perguntas, mas você só terá acesso à ultima mensagem
+      dele`
+
+    const personality = assistants.find(
+      (curr) => curr.name === assistant)?.personality
+
+    const response = await AIRequest(
+      JSON.stringify(question),
+      JSON.stringify(personality + ' ' + assistantInstructions) ?? '',
+      'prédio')
 
     return res.status(200).json(JSON.stringify(response.choices[0].message.content))
   }
 
   async getAssistants (req: Request, res: Response): Promise<Response> {
     const response = assistants.map((curr) => {
-      const { instructions, ...rest } = curr
+      const { personality, ...rest } = curr
       const image = fs.readFileSync(curr.profilePicPath)
       const base64Image = Buffer.from(image).toString('base64')
       const newObj: responseAssistant = {
