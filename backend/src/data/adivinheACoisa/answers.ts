73137e09d8e705fb265bd115f0ef2e21759386b2
GiovaniKill <giovanikill@hotmail.com>
2024-04-04 14:40:28 -0400

    feat: added some words to be answers

---
+++ backend/src/data/adivinheACoisa/answers.ts
@@ -0,0 +1,9 @@
+const answers: string[] = ['tomate', 'filho', 'prédio', 'copo', 'Toy Story', 'barraca', 'alergia', 'macarrão', 'bicicleta', 'sabonete',
+  'abacaxi', 'sorriso', 'avião', 'piano', 'computador', 'espelho', 'banana', 'dinossauro', 'melancia', 'chave', 'mochila', 'fantasma', 'navio', 'elefante',
+  'pirulito', 'violão', 'borboleta', 'foguete', 'aranha', 'castelo', 'sorvete', 'caneta', 'chapéu', 'pipoca', 'palhaço', 'cachorro', 'carro',
+  'bruxa', 'papagaio', 'cama', 'floresta', 'bola', 'dinheiro', 'pássaro', 'oceano', 'cadeira', 'espada', 'relógio', 'camisa', 'cachorro', 'gato', 'cachecol',
+  'teclado', 'telefone', 'árvore', 'lua', 'sunga', 'hamburguer', 'pizza', 'tigela', 'banheiro', 'cachoeira', 'livro', 'cadeado', 'fotografia', 'filme', 'série',
+  'vestido', 'sapato', 'óculos', 'bússola', 'bateria', 'coelho', 'pincel', 'martelo', 'faca', 'pote', 'tartaruga', 'piano', 'guitarra', 'violinista', 'teatro',
+  'microfone', 'câmera', 'gol', 'cemitério', 'zumbi', 'foguete', 'Lua']
+
+export default answers
