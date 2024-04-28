03a86fd4c814fc83c7b0fc851838dbc40199eb5f
giovanikill <giovanikill@hotmail.com>
2024-04-28 13:43:16 -0400

    fix: fixed sequelize env variable problem

--- backend/src/database/models/index.mjs
+++ backend/src/database/models/index.mjs
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import 'dotenv/config'
 import { Sequelize } from 'sequelize'
 import * as config from '../config/config.cjs'
 
-const index = new Sequelize(config)
+const index = new Sequelize(config[process.env.NODE_ENV])
 
 export default index
