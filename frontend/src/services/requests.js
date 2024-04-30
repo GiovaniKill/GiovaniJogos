e92281d26073e9362fa9b744ec160ddde18e99f4
giovanikill <giovanikill@hotmail.com>
2024-04-30 16:52:19 -0400

    feat: full google login and token verification

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -7,13 +7,32 @@ const api = axios.create({
   `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
-export const getRequest = async (endpoint) => {
-  const {data} = await api.get(endpoint, {withCredentials: true});
-  return data;
+/**
+ *
+ * @param {string} endpoint The URL of the requisition.
+ * @param {boolean} wholeResponse False if you want just the data,
+ * true for complete response.
+ * @returns {any}
+ */
+
+export const getRequest = async (endpoint, wholeResponse) => {
+  const response = await api.get(endpoint, {withCredentials: true});
+  if (wholeResponse) return response;
+  return response.data;
 };
 
-export const postRequest = async (endpoint, body) => {
-  const {data} = await api.post(endpoint, body, {withCredentials: true});
-  return data;
+/**
+ *
+ * @param {string} endpoint The URL of the requisition.
+ * @param {object} body The data to be sent.
+ * @param {boolean|undefined} wholeResponse False if you want just the data,
+ * true for complete response.
+ * @returns {any}
+ */
+
+export const postRequest = async (endpoint, body, wholeResponse) => {
+  const response = await api.post(endpoint, body, {withCredentials: true});
+  if (wholeResponse) return response;
+  return response.data;
 };
 
